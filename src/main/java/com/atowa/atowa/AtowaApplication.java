55c418eb01fad1fc381ea3b0b1026b3345bd123c
gauthier <gauthier.giles@epsi.fr>
2021-07-29 10:44:35 +0200

    First Commit to prepare website

---
+++ src/main/java/com/atowa/atowa/AtowaApplication.java
@@ -0,0 +1,13 @@
+package com.atowa.atowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtowaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtowaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atowa/atowa/AtowaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atowa.atowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtowaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
